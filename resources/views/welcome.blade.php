78dc5944b5eeb8d82512c51b4fef4c3fd1e840ad
July <1311102194@qq.com>
2018-03-07 16:46:50 +0800

    Update first welcome desige

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Design One.
                 </div>
 
                 <div class="links">
